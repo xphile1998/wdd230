e4deccdbc4db31e5cf3778717837f22d467331cc
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-04-22 11:53:54 -0700

    getting 24h time format

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ const today = new Date();
 const year = today.getFullYear();
 document.getElementById('copyYear').textContent = year;
 
-const options = {day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
+const options = {day: 'numeric', month: 'numeric', year: 'numeric', hour12: false, hour: '2-digit', minute: 'numeric', second: 'numeric'};
 document.getElementById('todaysDate').textContent = new Date().toLocaleDateString('en-us', options);
