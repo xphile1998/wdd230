dd199b1d50987daa3fe564d54dcce968712e4b87
xphile1998 <eron.whitefield@gmail.com>
2022-05-11 09:43:45 -0700

    Starting Week 4 assignment

---
+++ chamber/js/main.js
@@ -0,0 +1,7 @@
+const today = new Date();
+const year = today.getFullYear();
+document.getElementById('copyYear').textContent = year;
+
+// const options = {day: 'numeric', month: 'numeric', year: 'numeric', hour12: false, hour: '2-digit', minute: 'numeric', second: 'numeric'};
+// document.getElementById('todaysDate').textContent = new Date().toLocaleDateString('en-us', options);
+document.getElementById('lastModified').textContent = document.lastModified;
