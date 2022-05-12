a4d0d8b2aee640d1da7e61fadf546f50f4a77ed5
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-05-12 11:52:11 -0700

    Fixed insertion error...

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -1,10 +1,11 @@
 const today = new Date();
 const year = today.getFullYear();
 document.getElementById('copyYear').textContent = year;
-
+/*
 // const options = {day: 'numeric', month: 'numeric', year: 'numeric', hour12: false, hour: '2-digit', minute: 'numeric', second: 'numeric'};
 // document.getElementById('todaysDate').textContent = new Date().toLocaleDateString('en-us', options);
 document.getElementById('lastModified').textContent = document.lastModified;
+*/
 
 // select the elements to manipulate (output to)
 const datefieldUK = document.querySelector(".date");
