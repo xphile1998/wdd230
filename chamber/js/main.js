74bc05f5e40c5d3414b35b6be0b558e740e30627
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-05-14 20:36:54 -0700

    Content work

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -1,11 +1,10 @@
-const today = new Date();
-const year = today.getFullYear();
-document.getElementById('copyYear').textContent = year;
-/*
+// const today = new Date();
+// const year = today.getFullYear();
+// document.getElementById('copyYear').textContent = year;
+
 // const options = {day: 'numeric', month: 'numeric', year: 'numeric', hour12: false, hour: '2-digit', minute: 'numeric', second: 'numeric'};
 // document.getElementById('todaysDate').textContent = new Date().toLocaleDateString('en-us', options);
-document.getElementById('lastModified').textContent = document.lastModified;
-*/
+document.getElementById('lastModified').textContent = `Last Modification: ${document.lastModified}`;
 
 // display the date in UK style format: (day-of-the-week, day month year)
 
