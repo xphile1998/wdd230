85793a161380a439585b24c7348f5b03672685f0
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-05-14 21:27:47 -0700

    HTML content ready

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -1,14 +1,13 @@
-// const today = new Date();
-// const year = today.getFullYear();
-// document.getElementById('copyYear').textContent = year;
-
-// const options = {day: 'numeric', month: 'numeric', year: 'numeric', hour12: false, hour: '2-digit', minute: 'numeric', second: 'numeric'};
-// document.getElementById('todaysDate').textContent = new Date().toLocaleDateString('en-us', options);
+// Display the Last Modified Date of the page
 document.getElementById('lastModified').textContent = `Last Modification: ${document.lastModified}`;
 
 // display the date in UK style format: (day-of-the-week, day month year)
-
 const datefield = document.querySelector("#date");
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
 datefield.innerHTML = `<em>${fulldate}</em>`;
+
+// Display the copyright year
+const today = new Date();
+const year = today.getFullYear();
+document.getElementById('copyYear').textContent = year;
