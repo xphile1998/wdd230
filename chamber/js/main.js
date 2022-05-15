5d85a30357e3bc318432924688466c45cc9064d0
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-05-15 10:05:31 -0700

    Still working

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -13,3 +13,12 @@ const year = today.getFullYear();
 document.getElementById('copyYear').textContent = year;
 
 // Hamburger Button Click Behavior
+function toggleMenu() {
+    // console.log("It worked!");
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerBtn');
+
+x.onclick = toggleMenu;
