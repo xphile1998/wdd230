3158f492f70d3d664a8a82a4c05aaabcb6d5a82a
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-05-30 15:10:11 -0700

    Mostly done...

---
+++ lesson7/js/main.js
@@ -0,0 +1,39 @@
+﻿// get all images with the data-src attribute
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+// console.log(imagesToLoad);
+
+// optional parameters being set for the IntersectionalObserver
+/* const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+}; */
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+imagesToLoad.forEach((img) => {
+    loadImages(img);
+});
+
+// first check to see if Intersection Observer is supported
+/* if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            
+        });
+    }, imgOptions);
+    
+    // loop through each image and check status and load if necessary
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+    });
+}
+else { // just load all images if not supported
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+} */
