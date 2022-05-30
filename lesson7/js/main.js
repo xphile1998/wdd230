ea8e0d0c4501f5401eb978193b33c161c6148411
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-05-30 15:29:37 -0700

    Finished the assignment

--- lesson7/js/main.js
+++ lesson7/js/main.js
@@ -1,12 +1,11 @@
 ﻿// get all images with the data-src attribute
 let imagesToLoad = document.querySelectorAll('img[data-src]');
-// console.log(imagesToLoad);
 
 // optional parameters being set for the IntersectionalObserver
-/* const imgOptions = {
+const imgOptions = {
     threshold: 0,
     rootMargin: "0px 0px 50px 0px"
-}; */
+};
 
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
@@ -15,25 +14,26 @@ const loadImages = (image) => {
     };
 };
 
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-});
-
 // first check to see if Intersection Observer is supported
-/* if ('IntersectionObserver' in window) {
-    const imgObserver = new IntersectionObserver((items, observer) => {
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
         items.forEach((item) => {
-            
+           if (item.isIntersecting) {
+               loadImages(item.target);
+               observer.unobserve(item.target);
+           } 
         });
     }, imgOptions);
     
     // loop through each image and check status and load if necessary
     imagesToLoad.forEach((img) => {
-        imgObserver.observe(img);
+        observer.observe(img);
+        console.log('Completed normally.')
     });
 }
 else { // just load all images if not supported
     imagesToLoad.forEach((img) => {
         loadImages(img);
+        console.log('Completed abnormally.')
     });
-} */
+} 
