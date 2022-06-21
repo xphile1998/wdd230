1d10109c3bf59c97e0b7b253b59256eeef4dc36b
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-06-21 14:30:19 -0700

    Directory page is mostly complete. Need to add the display choice still.

---
+++ chamber/js/directory.js
@@ -0,0 +1,53 @@
+﻿const requestURL = 'json/data.json';
+// const cards = document.querySelector('.cards');
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject);
+
+        const businesses = jsonObject['businesses'];
+        businesses.forEach(displayBusinesses);
+    });
+
+function displayBusinesses(business) {
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let logo = document.createElement('img');
+    let bname = document.createElement('h2');
+    let bdescription = document.createElement('h3');
+    let cname = document.createElement('p');
+    let title = document.createElement('p');
+    let phone = document.createElement('p');
+    let email = document.createElement('p');
+    let joined = document.createElement('h4');
+    
+    // Change the textContent property of the h2 element to contain the prophet's full name
+    bname.innerHTML = `${business.business_name}`
+    bdescription.innerHTML = `${business.business_description}`
+    cname.innerHTML = `Contact Name: ${business.first_name}  ${business.last_name}`;
+    title.innerHTML = `Contact Title: ${business.contact_title}`
+    phone.innerHTML = `Phone: ${business.contact_phone}`;
+    email.innerHTML = `Email: ${business.contact_email}`;
+    joined.innerHTML = `${business.business_name} joined the Chamber in ${business.joined}!`
+
+    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+    logo.setAttribute('src', business.logo);
+    logo.setAttribute('alt', `Logo of ${business.business_name}`);
+    logo.setAttribute('loading', 'lazy');
+
+    // Add/append the section(card) with the h2 element
+    card.appendChild(logo);
+    card.appendChild(bname);
+    card.appendChild(bdescription);
+    card.appendChild(cname);
+    card.appendChild(title);
+    card.appendChild(phone);
+    card.appendChild(email)
+    card.appendChild(joined);
+
+    // Add/append the existing HTML div with the cards class with the section(card)
+    document.querySelector('div.cards').appendChild(card);
+}
---
+++ chamber/js/wireframes.js
@@ -0,0 +1,35 @@
+﻿const requestURL = 'json/wireframes.json';
+// const cards = document.querySelector('.cards');
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject);
+
+        const wireframes = jsonObject['wireframes'];
+        wireframes.forEach(displayWireframes);
+    });
+
+function displayWireframes(wireframe) {
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let picture = document.createElement('img');
+    let description = document.createElement('h2');
+    
+    // Change the textContent property of the h2 element to contain the prophet's full name
+    description.innerHTML = `${wireframe.description}`
+    
+    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+    picture.setAttribute('src', wireframe.picURL);
+    picture.setAttribute('alt', `${wireframe.description}`);
+    picture.setAttribute('loading', 'lazy');
+
+    // Add/append the section(card) with the h2 element
+    card.appendChild(description);
+    card.appendChild(picture);
+    
+    // Add/append the existing HTML div with the cards class with the section(card)
+    document.querySelector('div.cards').appendChild(card);
+}
