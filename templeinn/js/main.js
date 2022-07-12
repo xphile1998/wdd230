d254a8feb18042cca2b55a590c7f78397a8663fc
xphile1998 <eron.whitefield@gmail.com>
2022-07-11 18:05:19 -0700

    Day 1 work completed

--- templeinn/js/main.js
+++ templeinn/js/main.js
@@ -1,3 +1,21 @@
+// Display the Last Modified Date of the page
+document.getElementById('lastModified').textContent = `Last Modification: ${document.lastModified}`;
+
+// display the date in UK style format: (day-of-the-week, day month year)
+/*
+const datefield = document.querySelector("#date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
+datefield.innerHTML = `<em>${fulldate}</em>`;
+const todayDisplay = document.querySelector("#today");
+*/
+
+// Display the copyright year
+const today = new Date();
+const year = today.getFullYear();
+// console.log(year);
+document.getElementById("copyYear").textContent = year;
+
 // Hamburger Button Click Behavior
 function toggleMenu() {
     // console.log("It worked!");
@@ -8,19 +26,3 @@ function toggleMenu() {
 const x = document.getElementById('hamburgerBtn');
 
 x.onclick = toggleMenu;
-
-// Display the Last Modified Date of the page
-document.getElementById('lastModified').textContent = `Last Modification: ${document.lastModified}`;
-
-// display the date in UK style format: (day-of-the-week, day month year)
-const datefield = document.querySelector("#date");
-const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
-datefield.innerHTML = `<em>${fulldate}</em>`;
-const todayDisplay = document.querySelector("#today");
-
-// Display the copyright year
-const today = new Date();
-const year = today.getFullYear();
-document.getElementById('copyYear').textContent = year;
-
