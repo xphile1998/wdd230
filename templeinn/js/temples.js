5b3f196497dff1de8f5ddf60e0f9c18426c094f4
xphile1998 <77454377+xphile1998@users.noreply.github.com>
2022-07-18 19:20:42 -0700

    Updating for more Day 4 stuff

--- templeinn/js/temples.js
+++ templeinn/js/temples.js
@@ -9,44 +9,75 @@ fetch(requestURL)
         console.table(jsonObject);
 
         const temples = jsonObject['templeList'];
-        temples.forEach(displayBusinesses);
+        temples.forEach(displayTemples);
     });
 
 function displayTemples(temple) {
     // Create elements to add to the document
     let card = document.createElement('section');
-    let pic = document.createElement('img');
-    let tname = document.createElement('h2'); 
-    let bdescription = document.createElement('h3');
-    let cname = document.createElement('p');
-    let title = document.createElement('p');
-    let phone = document.createElement('p');
-    let email = document.createElement('p');
-    let joined = document.createElement('h4');
-
-    // Change the textContent property of the h2 element to contain the prophet's full name
-    bname.innerHTML = `${business.business_name}`
-    bdescription.innerHTML = `${business.business_description}`
-    cname.innerHTML = `Contact Name: ${business.first_name}  ${business.last_name}`;
-    title.innerHTML = `Contact Title: ${business.contact_title}`
-    phone.innerHTML = `Phone: ${business.contact_phone}`;
-    email.innerHTML = `Email: ${business.contact_email}`;
-    joined.innerHTML = `${business.business_name} joined the Chamber in ${business.joined}!`
-
+    let templePic = document.createElement('img');
+    let name = document.createElement('h2'); 
+    let address = document.createElement('div');
+    let contact = document.createElement('div')
+    let schedules = document.createElement('div');
+    let services = document.createElement('div');
+    let history = document.createElement('div');
+        
+    // Change the textContent property of the elements
+    name.innerHTML = `${temple.name}`;
+    address.innerHTML = `<p>${temple.address.street}<br>
+                         ${temple.address.city}, ${temple.address.state} ${temple.address.zip}<br>
+                         ${temple.address.country}</p>`;
+    contact.innerHTML = `<legend class="templeCardLeg">Contact Information</legend>
+                         <p class="templePara">Phone: ${temple.contact.phone}<br>
+                         Email: ${temple.contact.email}</p>`;
+    schedules.innerHTML = `<fieldset class="templeCard">
+                                <legend class="templeCardLeg">Schedules</legend>
+                                <p class="templePara">Ordinances: ${temple.schedules.ordinanceSchedule}</p>
+                                <p class="templePara">Sessions: ${temple.schedules.sessionSchedule}</p>
+                            </fieldset>
+                            <fieldset class="templeCard">
+                                <legend class="templeCardLeg">Closure Schedule</legend>
+                                <ul>
+                                    <li class=templeLi>${temple.schedules.closureSchedule.closure1}</li>
+                                    <li class=templeLi>${temple.schedules.closureSchedule.closure2}</li>
+                                    <li class=templeLi>${temple.schedules.closureSchedule.closure3}</li>
+                                    <li class=templeLi>${temple.schedules.closureSchedule.closure4}</li>
+                                    <li class=templeLi>${temple.schedules.closureSchedule.closure5}</li>
+                                </ul>
+                            </fieldset>`
+    services.innerHTML = `<legend class="templeCardLeg">Temple Services</legend>
+                          <p class="templePara">${temple.services.service1}<br>
+                          ${temple.services.service2}<br>
+                          ${temple.services.service3}<br>
+                          ${temple.services.service4}</p>`;
+    history.innerHTML = `<legend class="templeCardLeg">Temple History</legend> 
+                         <p class="templePara">${temple.history.announced}<br>
+                         Groundbreaking: ${temple.history.groundbreaking}<br>
+                         Dedication: ${temple.history.dedication}</p>`;
+    
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    logo.setAttribute('src', business.logo);
-    logo.setAttribute('alt', `Logo of ${business.business_name}`);
-    logo.setAttribute('loading', 'lazy');
-
+    templePic.setAttribute('class', 'templePic');
+    templePic.setAttribute('src', temple.templePicURL);
+    templePic.setAttribute('alt', `Picture of the ${temple.name} temple.`);
+    templePic.setAttribute('loading', 'lazy');
+    
+    card.setAttribute('class', 'tempCard');
+    name.setAttribute('class', 'name');
+    address.setAttribute('class', 'address');
+    contact.setAttribute('class', 'contact');
+    schedules.setAttribute('class', 'schedules');
+    services.setAttribute('class', 'services');
+    history.setAttribute('class', 'history');
+        
     // Add/append the section(card) with the h2 element
-    card.appendChild(logo);
-    card.appendChild(bname);
-    card.appendChild(bdescription);
-    card.appendChild(cname);
-    card.appendChild(title);
-    card.appendChild(phone);
-    card.appendChild(email)
-    card.appendChild(joined);
+    card.appendChild(templePic);
+    card.appendChild(name);
+    card.appendChild(address);
+    card.appendChild(contact);
+    card.appendChild(schedules);
+    card.appendChild(services);
+    card.appendChild(history);
 
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector('div.cards').appendChild(card);
